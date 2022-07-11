f6ce04f5f4d1eba819fd2f5d11eacf19f65a461e
Theo Morales <theo.morales.fr@gmail.com>
2022-07-11 16:00:22 +0100

    Add training script

---
+++ vm_train.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# vm_train.sh
+# Copyright (C) 2022 Théo Morales <theo.morales.fr@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+source venv/bin/activate && python code/model_train.py
