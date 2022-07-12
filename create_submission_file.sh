337c0164689f075bce7fdf578bbd1c34a27cae81
Theo Morales <theo.morales.fr@gmail.com>
2022-07-12 17:36:15 +0100

    Lighter submission file

--- create_submission_file.sh
+++ create_submission_file.sh
@@ -1,4 +1,4 @@
 # Let's create a .ZIP with the necessary files for the submission
-zip -r submission.zip Dockerfile code results
+zip -r submission.zip Dockerfile code results/models/visum2022.pt
 
 echo "Submission file created."
